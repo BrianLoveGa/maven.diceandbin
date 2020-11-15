0f0bbadf31d3229f448a76e27a6228dd4ccfda9f
BLoveless <bloveless@talentpath.com>
2020-11-15 16:08:12 -0500

    dice class passed

--- src/main/java/com/github/curriculeon/Dice.java
+++ src/main/java/com/github/curriculeon/Dice.java
@@ -2,6 +2,8 @@ package com.github.curriculeon;
 
 public class Dice {
 
+    private final Die[] dice;
+
     /**
      * @param numberOfDie - number of die-objects to be contained
      */
@@ -14,6 +16,13 @@ public class Dice {
      * @param numberOfFaces - number of faces on a single die-object
      */
     public Dice(Integer numberOfDie, Integer numberOfFaces) {
+        this.dice = new Die[numberOfDie];
+
+        for (int i = 0; i < dice.length; i++){
+            Die die = new Die(numberOfFaces);
+            dice[i]= die;
+        }
+
     }
 
     /**
@@ -21,20 +30,34 @@ public class Dice {
      * sum all of the random numbers up and that equals total for that roll
      */
     public Integer rollAndSum() {
-        return null;
+        Integer total = 0;
+
+        for (int j = 0; j < dice.length; j++){
+            Die currentElement = dice[j];
+            currentElement.roll();
+            int youRolledValue = currentElement.getCurrentFaceValue();
+            total += youRolledValue;
+        }
+        return total;
     }
 
     /**
      * @return the absolute minimum value that can be rolled by this set of die
      */
     public Integer getRollMin() {
-        return null;
+        Integer numberOfDie = dice.length;
+        Integer minimumValueToBeRolled = numberOfDie;
+        return minimumValueToBeRolled;
     }
 
     /**
      * @return the absolute maximum value that can be rolled by this set of die
      */
     public Integer getRollMax() {
-        return null;
+        Die oneOfTheDie = dice[0]; // look at one of the die
+        Integer numberOfFaces = oneOfTheDie.getNumberOfFaces(); // count number of faces
+        Integer numberOfDie = dice.length; // count number of die
+        Integer maximumValueToBeRolled = numberOfFaces * numberOfDie;
+        return maximumValueToBeRolled;
     }
 }
