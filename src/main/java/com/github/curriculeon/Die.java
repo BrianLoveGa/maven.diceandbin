10ecc89e732af0b1192e095da3552f01e98730d5
BLoveless <bloveless@talentpath.com>
2020-11-15 16:00:11 -0500

    die done

--- src/main/java/com/github/curriculeon/Die.java
+++ src/main/java/com/github/curriculeon/Die.java
@@ -1,18 +1,31 @@
 package com.github.curriculeon;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Die {
+
+    private Integer numberOfFaces;
+    private Integer currentFaceValue;
+
+
     public Die() {
+        this(6);
     }
 
     public Die(Integer numberOfFaces) {
+        this.numberOfFaces = numberOfFaces;
     }
 
     public void roll() {
+        ThreadLocalRandom randNumbGen = ThreadLocalRandom.current();
+        this.currentFaceValue = randNumbGen.nextInt(1,numberOfFaces);
     }
 
     public Integer getCurrentFaceValue() {
-        return null;    }
+        return currentFaceValue;    }
 
     public Integer getNumberOfFaces() {
-        return null;    }
+        return numberOfFaces;    }
 }
+
+/// laissez les bons temps rouler !!!
