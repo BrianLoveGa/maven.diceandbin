5380bc3621d3e66bfd3cdfac66ba6144e8860450
BLoveless <bloveless@talentpath.com>
2020-11-15 16:27:14 -0500

    should be right but something wrong, never tosss an 11 or 12...

--- src/main/java/com/github/curriculeon/MainApplication.java
+++ src/main/java/com/github/curriculeon/MainApplication.java
@@ -2,7 +2,7 @@ package com.github.curriculeon;
 
 public class MainApplication {
     public static void main(String[] args) {
-        Simulation sim = new Simulation(2, 1000000);
+        Simulation sim = new Simulation(2, 100);
         sim.run();
         String output = sim.toString();
         System.out.println(output);
--- src/main/java/com/github/curriculeon/Simulation.java
+++ src/main/java/com/github/curriculeon/Simulation.java
@@ -1,23 +1,53 @@
 package com.github.curriculeon;
 
+import java.util.Formatter;
+
 public class Simulation {
 
+
+    private Integer numberOfDie;
+    private Integer numberOfTosses;
+    private Bins bins;
+
     public Simulation(Integer numberOfDie, Integer numberOfTosses) {
+        this.numberOfDie = numberOfDie;
+        this.numberOfTosses = numberOfTosses;
     }
 
     public void run() {
+        Dice dice = new Dice(numberOfDie);
+        bins = new Bins (dice.getRollMin(), dice.getRollMax());
+        for (int i=0; i < numberOfTosses; i++){
+            Integer faceValue = dice.rollAndSum();
+            bins.incrementBin(faceValue);
+        }
     }
 
     public Double getPercentageOfOccurrences(Integer faceValueToCheck) {
-        return null;
+        Bin bin = bins.getBin(faceValueToCheck);
+        Integer numberOfOccurrences = bin.getNumberOfOccurrences();
+        Double ratio = numberOfOccurrences.doubleValue() / numberOfTosses;
+        return ratio * 100;     ///// < ---- to make a percentage * 100 here  --- //////
     }
 
     @Override
     public String toString() {
-        return null;
+        Formatter outPut = new Formatter();
+        for(Bin bin : bins.getBins()) {
+            // doing math
+            Integer faceValue = bin.getFaceValueToTrack();
+            Integer numberOfOccurrences = bin.getNumberOfOccurrences();
+            Double percentage = getPercentageOfOccurrences(faceValue);
+
+            // formatting string
+            Integer starCount = percentage.intValue();
+            String stars = new String(new char[starCount]).replace("\0", "*");
+            outPut.format("%d : %d: %.2f: %s%n", faceValue, numberOfOccurrences, percentage, stars);
+        }
+        return outPut.toString();
     }
 
     public Bins getBins() {
-        return null;
+        return bins;
     }
 }
